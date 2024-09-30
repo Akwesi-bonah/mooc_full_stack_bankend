5483450945d83bf5c08f37e83d31dca109b1bd3b
Akwesi-Bonah <arhinbonnah@gmail.com>
2024-09-30 19:14:15 +0000

    update

--- models/person.js
+++ models/person.js
@@ -13,11 +13,25 @@ mongoose
     console.log("error connecting to MongoDB:", error.message);
   });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number! It should be in the format 09-1234556 or 040-22334455.`
+      },
+      required: [true, 'User phone number is required']
+    }
+  });
+  
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
